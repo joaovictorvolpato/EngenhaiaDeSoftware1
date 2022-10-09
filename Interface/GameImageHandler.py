fb9a5da6325068cf6a204aeba62d287022baea62
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-10-09 16:38:04 -0300

    Fix PhotoImage created before TK's window bug

--- Interface/GameImageHandler.py
+++ Interface/GameImageHandler.py
@@ -2,24 +2,23 @@ from tkinter import PhotoImage
 from GamePath import GamePath
 
 class GameImageHandler:
-    canvas_image = PhotoImage(
-        file=GamePath.relative_to_assets("canvas_image.png")
-        )
+    """Stores all images used in the game. Photoimages must be
+    set only after the window is created, so the set method is only 
+    called after the GameInterface instantiates TKinter's window."""
+    canvas_image = None
+    button_image = None
+    AVAI_piece_image = None
+    VASCO_piece_image = None
+    current_piece_image = None
 
-    button_image = PhotoImage(
-        file = GamePath.relative_to_assets("button_image.png")
-        )
-
-    AVAI_piece_image = PhotoImage(
-        file=GamePath.relative_to_assets("AVAI.png")
-        )
-
-    VASCO_piece_image = PhotoImage(
-        file = GamePath.relative_to_assets("VASCO.png")
-        )   
-
-    current_piece_image = VASCO_piece_image
+    @classmethod
+    def set_game_images(cls) -> None:
+        cls.canvas_image = PhotoImage(file=GamePath.relative_to_assets("canvas_image.png"))
+        cls.button_image = PhotoImage(file = GamePath.relative_to_assets("button_image.png"))
+        cls.AVAI_piece_image = PhotoImage(file=GamePath.relative_to_assets("AVAI.png"))
+        cls.VASCO_piece_image = PhotoImage(file = GamePath.relative_to_assets("VASCO.png"))   
+        cls.current_piece_image = cls.VASCO_piece_image
 
     @classmethod
     def set_current_piece_image(cls, piece_image:PhotoImage) -> None:
-        GameImageHandler.current_piece_image = piece_image
+        cls.current_piece_image = piece_image
