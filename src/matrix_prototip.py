84a05b7382813c208db8fe6eb46c80e150ee4850
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-11-11 01:09:02 -0300

    evaluateMoinho and verifyBlocked algorithm diagrams

---
+++ src/matrix_prototip.py
@@ -0,0 +1,7 @@
+board_matrix = [[None, 'p1', 'p2', 'p3', None, None, None],
+                [None, None, 'p4', 'p5', 'p6', None, 'p7'], 
+                [None, 'p8', 'p9', 'p10', 'p11', 'p12', 'p13'], 
+                ['p14', 'p15', 'p16', None, 'p17', 'p18', 'p19'], 
+                ['p20', 'p21', 'p22', 'p23', 'p24', 'p25', None], 
+                ['p26', None, 'p27', 'p28', 'p29', None, None],
+                [None, None, None, 'p30', 'p31', 'p32', None]]
