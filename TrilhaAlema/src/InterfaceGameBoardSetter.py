02517124424d8349e0616200c8835a45e0ca31f0
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-26 17:07:31 -0300

    Mapped switches id to position in board

--- TrilhaAlema/src/InterfaceGameBoardSetter.py
+++ TrilhaAlema/src/InterfaceGameBoardSetter.py
@@ -98,47 +98,62 @@ class InterfaceGameBoardSetter:
         )
 
     def __setup_piece_switches(self) -> None: #Change piece switches names to match the board places sequence. Code on click to return the button number
-        piece_switch_1 = PieceSwitch(
-            id = 1,
+        piece_switch_32 = PieceSwitch(
+            id = 32,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_1.on_click(),
+            command=lambda: piece_switch_32.on_click(),
             relief="flat"
         )
-        piece_switch_1.place(
+        piece_switch_32.place(
             x=662.0,
             y=691.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_2 = PieceSwitch(
-            id = 2,
+        piece_switch_20 = PieceSwitch(
+            id = 20,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_2.on_click(),
+            command=lambda: piece_switch_20.on_click(),
             relief="flat"
         )
-        piece_switch_2.place(
+        piece_switch_20.place(
             x=162.0,
             y=491.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_3 = PieceSwitch(
-            id = 3,
+        piece_switch_26 = PieceSwitch(
+            id = 26,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_3.on_click(),
+            command=lambda: piece_switch_26.on_click(),
             relief="flat"
         )
-        piece_switch_3.place(
+        piece_switch_26.place(
             x=162.0,
             y=591.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_9 = PieceSwitch(
+            id = 9,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_9.on_click(),
+            relief="flat"
+        )
+        piece_switch_9.place(
+            x=362.0,
+            y=291.0,
             width=50.0,
             height=50.0
         )
@@ -153,52 +168,22 @@ class InterfaceGameBoardSetter:
         )
         piece_switch_4.place(
             x=362.0,
-            y=291.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_5 = PieceSwitch(
-            id = 5,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_5.on_click(),
-            relief="flat"
-        )
-        piece_switch_5.place(
-            x=362.0,
             y=191.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_6 = PieceSwitch(
-            id = 6,
+        piece_switch_1 = PieceSwitch(
+            id = 1,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda:piece_switch_6.on_click(),
+            command=lambda:piece_switch_1.on_click(),
             relief="flat"
         )
-        piece_switch_6.place(
+        piece_switch_1.place(
             x=262.0,
             y=100.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_7 = PieceSwitch(
-            id = 7,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_7.on_click(),
-            relief="flat"
-        )
-        piece_switch_7.place(
-            x=262.0,
-            y=291.0,
             width=50.0,
             height=50.0
         )
@@ -213,97 +198,7 @@ class InterfaceGameBoardSetter:
         )
         piece_switch_8.place(
             x=262.0,
-            y=391.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_9 = PieceSwitch(
-            id = 9,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_9.on_click(),
-            relief="flat"
-        )
-        piece_switch_9.place(
-            x=262.0,
-            y=491.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_10 = PieceSwitch(
-            id = 10,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_10.on_click(),
-            relief="flat"
-        )
-        piece_switch_10.place(
-            x=562.0,
-            y=691.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_11 = PieceSwitch(
-            id = 11,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_11.on_click(),
-            relief="flat"
-        )
-        piece_switch_11.place(
-            x=162.0,
-            y=391.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_12 = PieceSwitch(
-            id = 12,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_12.on_click(),
-            relief="flat"
-        )
-        piece_switch_12.place(
-            x=462.0,
-            y=691.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_13 = PieceSwitch(
-            id = 13,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_13.on_click(),
-            relief="flat"
-        )
-        piece_switch_13.place(
-            x=362.0,
-            y=391.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_14 = PieceSwitch(
-            id = 14,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_14.on_click(),
-            relief="flat"
-        )
-        piece_switch_14.place(
-            x=462.0,
-            y=491.0,
+            y=291.0,
             width=50.0,
             height=50.0
         )
@@ -317,8 +212,68 @@ class InterfaceGameBoardSetter:
             relief="flat"
         )
         piece_switch_15.place(
+            x=262.0,
+            y=391.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_21 = PieceSwitch(
+            id = 21,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_21.on_click(),
+            relief="flat"
+        )
+        piece_switch_21.place(
+            x=262.0,
+            y=491.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_31 = PieceSwitch(
+            id = 31,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_31.on_click(),
+            relief="flat"
+        )
+        piece_switch_31.place(
             x=562.0,
-            y=591.0,
+            y=691.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_14 = PieceSwitch(
+            id = 14,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_14.on_click(),
+            relief="flat"
+        )
+        piece_switch_14.place(
+            x=162.0,
+            y=391.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_30 = PieceSwitch(
+            id = 30,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_30.on_click(),
+            relief="flat"
+        )
+        piece_switch_30.place(
+            x=462.0,
+            y=691.0,
             width=50.0,
             height=50.0
         )
@@ -332,36 +287,81 @@ class InterfaceGameBoardSetter:
             relief="flat"
         )
         piece_switch_16.place(
+            x=362.0,
+            y=391.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_23 = PieceSwitch(
+            id = 23,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_23.on_click(),
+            relief="flat"
+        )
+        piece_switch_23.place(
+            x=462.0,
+            y=491.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_29 = PieceSwitch(
+            id = 29,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_29.on_click(),
+            relief="flat"
+        )
+        piece_switch_29.place(
+            x=562.0,
+            y=591.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_12 = PieceSwitch(
+            id = 12,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_12.on_click(),
+            relief="flat"
+        )
+        piece_switch_12.place(
             x=662.0,
             y=291.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_17 = PieceSwitch(
-            id = 17,
+        piece_switch_13 = PieceSwitch(
+            id = 13,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_17.on_click(),
+            command=lambda: piece_switch_13.on_click(),
             relief="flat"
         )
-        piece_switch_17.place(
+        piece_switch_13.place(
             x=762.0,
             y=291.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_18 = PieceSwitch(
-            id = 18,
+        piece_switch_7 = PieceSwitch(
+            id = 7,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_18.on_click(),
+            command=lambda: piece_switch_7.on_click(),
             relief="flat"
         )
-        piece_switch_18.place(
+        piece_switch_7.place(
             x=762.0,
             y=191.0,
             width=50.0,
@@ -383,62 +383,47 @@ class InterfaceGameBoardSetter:
             height=50.0
         )
 
-        piece_switch_20 = PieceSwitch(
-            id = 20,
+        piece_switch_28 = PieceSwitch(
+            id = 28,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_20.on_click(),
+            command=lambda: piece_switch_28.on_click(),
             relief="flat"
         )
-        piece_switch_20.place(
+        piece_switch_28.place(
             x=462.0,
             y=591.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_21 = PieceSwitch(
-            id = 21,
+        piece_switch_25= PieceSwitch(
+            id = 25,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_21.on_click(),
+            command=lambda: piece_switch_25.on_click(),
             relief="flat"
         )
-        piece_switch_21.place(
+        piece_switch_25.place(
             x=662.0,
             y=491.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_22 = PieceSwitch(
-            id = 22,
+        piece_switch_18 = PieceSwitch(
+            id = 18,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_22.on_click(),
+            command=lambda: piece_switch_18.on_click(),
             relief="flat"
         )
-        piece_switch_22.place(
+        piece_switch_18.place(
             x=662.0,
             y=391.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_23 = PieceSwitch(
-            id = 23,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_23.on_click(),
-            relief="flat"
-        )
-        piece_switch_23.place(
-            x=562.0,
-            y=491.0,
             width=50.0,
             height=50.0
         )
@@ -452,38 +437,128 @@ class InterfaceGameBoardSetter:
             relief="flat"
         )
         piece_switch_24.place(
+            x=562.0,
+            y=491.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_10 = PieceSwitch(
+            id = 10,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_10.on_click(),
+            relief="flat"
+        )
+        piece_switch_10.place(
             x=462.0,
             y=291.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_25 = PieceSwitch(
-            id = 25,
+        piece_switch_5 = PieceSwitch(
+            id = 5,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_25.on_click(),
+            command=lambda: piece_switch_5.on_click(),
             relief="flat"
         )
-        piece_switch_25.place(
+        piece_switch_5.place(
             x=462.0,
             y=191.0,
             width=50.0,
             height=50.0
         )
 
-        piece_switch_26 = PieceSwitch(
-            id = 26,
+        piece_switch_3 = PieceSwitch(
+            id = 3,
             image=GameImageHandler.button_image,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: piece_switch_26.on_click(),
+            command=lambda: piece_switch_3.on_click(),
             relief="flat"
         )
-        piece_switch_26.place(
+        piece_switch_3.place(
             x=461.0,
             y=100.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_2 = PieceSwitch(
+            id = 2,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_2.on_click(),
+            relief="flat"
+        )
+        piece_switch_2.place(
+            x=362.0,
+            y=100.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_11 = PieceSwitch(
+            id = 11,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_11.on_click(),
+            relief="flat"
+        )
+        piece_switch_11.place(
+            x=562.0,
+            y=291.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_6 = PieceSwitch(
+            id = 6,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_6.on_click(),
+            relief="flat"
+        )
+        piece_switch_6.place(
+            x=562.0,
+            y=191.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_17 = PieceSwitch(
+            id = 17,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_17.on_click(),
+            relief="flat"
+        )
+        piece_switch_17.place(
+            x=562.0,
+            y=391.0,
+            width=50.0,
+            height=50.0
+        )
+
+        piece_switch_22 = PieceSwitch(
+            id = 22,
+            image=GameImageHandler.button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: piece_switch_22.on_click(),
+            relief="flat"
+        )
+        piece_switch_22.place(
+            x=362.0,
+            y=491.0,
             width=50.0,
             height=50.0
         )
@@ -497,81 +572,6 @@ class InterfaceGameBoardSetter:
             relief="flat"
         )
         piece_switch_27.place(
-            x=362.0,
-            y=100.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_28 = PieceSwitch(
-            id = 28,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_28.on_click(),
-            relief="flat"
-        )
-        piece_switch_28.place(
-            x=562.0,
-            y=291.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_29 = PieceSwitch(
-            id = 29,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_29.on_click(),
-            relief="flat"
-        )
-        piece_switch_29.place(
-            x=562.0,
-            y=191.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_30 = PieceSwitch(
-            id = 30,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_30.on_click(),
-            relief="flat"
-        )
-        piece_switch_30.place(
-            x=562.0,
-            y=391.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_31 = PieceSwitch(
-            id = 31,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_31.on_click(),
-            relief="flat"
-        )
-        piece_switch_31.place(
-            x=362.0,
-            y=491.0,
-            width=50.0,
-            height=50.0
-        )
-
-        piece_switch_32 = PieceSwitch(
-            id = 32,
-            image=GameImageHandler.button_image,
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: piece_switch_32.on_click(),
-            relief="flat"
-        )
-        piece_switch_32.place(
             x=362.0,
             y=591.0,
             width=50.0,
--- TrilhaAlema/src/PieceSwitch.py
+++ TrilhaAlema/src/PieceSwitch.py
@@ -1,15 +1,15 @@
-from tkinter import Button
+from tkinter import Button, PhotoImage
 from GameImageHandler import GameImageHandler
 
 class PieceSwitch(Button):
-    def __init__(self, id, image, borderwidth, highlightthickness, command, relief):
+    def __init__(self, id:int, image:PhotoImage, borderwidth:int, highlightthickness:int, command, relief:str) -> None:
         Button.__init__(self, image = image, borderwidth = borderwidth, highlightthickness = highlightthickness, command = command, relief = relief)
         self.button_image = GameImageHandler.button_image
-        self.button_pressed = False
-        self.name = id
+        self.button_pressed:bool = False
+        self.id = id
 
     def on_click(self) -> None:
-        print(self.name)
+        print(self.id)
         if not self.button_pressed:
             self.configure(image = GameImageHandler.current_piece_image)
             self.set_button_pressed()
