2ff0b6686fe3d4e52c8887b35248ba4be3740548
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-27 02:19:10 -0300

    Finished BoardCanvas

--- TrilhaAlema/src/Interface/BoardCanvas.py
+++ TrilhaAlema/src/Interface/BoardCanvas.py
@@ -1,89 +1,87 @@
-from tkinter import Canvas, Tk
+from tkinter import Canvas, Tk, PhotoImage, _CanvasItemId
+from Interface.GameImageHandler import GameImageHandler
 
 class BoardCanvas(Canvas):
     def __init__(self, window:Tk, bg:str, height:int, width:int, bd:int, highlightthickness:int, relief:str) -> None:
-        Canvas.__init__(self, master, width = width, height = height, bg = bg, highlightthickness = highlightthickness, bd = bd, relief = relief)
+        Canvas.__init__(self, window = window, width = width, height = height, bg = bg, highlightthickness = highlightthickness, bd = bd, relief = relief)
+        self.draw_image_on_canvas(492.0, 428.0, GameImageHandler.canvas_image)
+        self.draw_button_text = self.__write_on_canvas(203.0, 781.0, anchor="nw", text="Click to Propose Draw", fill="#FFFFFF", font=("Inter", 32 * -1))
+        self.__local_player_info, self.__remote_player_info = self.create_players_info()
+        self.__local_player_text, self.__remote_player_text = self.write_player_texts()
+        self.__local_player_pieces_in_hand, self.__remote_player_pieces_in_hand = self.write_players_pieces_in_hand()
+        self.__local_player_captured_pieces, self.__remote_player_captured_pieces = self.write_players_captured_pieces()
 
-    def draw_rectangle(self, position:tuple, position2:tuple, fill:str, width:int) -> None:
-        self.create_rectangle(position, position2, fill = fill, width = width)
+    @property
+    def local_player_pieces_in_hand(self) -> _CanvasItemId:
+        return self.__local_player_pieces_in_hand
 
-        def __setup_canvas(self) -> None:
-        canvas = Canvas(
-            self.__window,
-            bg = "#327421",
-            height = 1024,
-            width = 1440,
-            bd = 0,
-            highlightthickness = 0,
-            relief = "ridge"
-        )
-        canvas.place(x = 0, y = 0)
+    @local_player_pieces_in_hand.setter
+    def local_player_pieces_in_hand(self, pieces_in_hand:int) -> None:
+        self.change_text(f"Pieces In Hand: {pieces_in_hand}", self.__local_player_pieces_in_hand)
+    
+    @property
+    def remote_player_pieces_in_hand(self) -> _CanvasItemId:
+        return self.__remote_player_pieces_in_hand
 
-        board_image = canvas.create_image(
-            492.0,
-            428.0,
-            image=GameImageHandler.canvas_image
-        )
+    @remote_player_pieces_in_hand.setter
+    def remote_player_pieces_in_hand(self, pieces_in_hand:int) -> None:
+        self.change_text(f"Pieces In Hand: {pieces_in_hand}", self.__local_player_pieces_in_hand)
 
-        canvas.create_rectangle(
-            989.0,
-            80.0,
-            1194.0,
-            756.0,
-            fill="#FFFFFF",
-            outline="")
+    @property
+    def local_player_captured_pieces(self) -> _CanvasItemId:
+        return self.__local_player_captured_pieces
 
-        canvas.create_rectangle(
-            1214.0,
-            80.0,
-            1419.0,
-            756.0,
-            fill="#FFFFFF",
-            outline="")
+    @local_player_captured_pieces.setter
+    def local_player_captured_pieces(self, captured_pieces:int) -> None:
+        self.change_text(f"Captured Pieces: {captured_pieces}", self.__local_player_captured_pieces)
+    
+    @property
+    def remote_player_captured_pieces(self) -> _CanvasItemId:
+        return self.__remote_player_captured_pieces
 
-        canvas.create_text(
-            1238.0,
-            104.0,
-            anchor="nw",
-            text="Opponent",
-            fill="#000000",
-            font=("Inter", 32 * -1)
-        )
+    @remote_player_captured_pieces.setter
+    def remote_player_captured_pieces(self, captured_pieces:int) -> None:
+        self.change_text(f"Captured Pieces: {captured_pieces}", self.__remote_player_captured_pieces)
 
-        canvas.create_text(
-            1007.0,
-            105.0,
-            anchor="nw",
-            text="You",
-            fill="#000000",
-            font=("Inter", 32 * -1)
-        )
-
-        canvas.create_text(
-            203.0,
-            781.0,
-            anchor="nw",
-            text="Click to Propose Draw",
-            fill="#FFFFFF",
-            font=("Inter", 32 * -1)
-        )
-
-        canvas.create_rectangle(
-            479.0,
-            862.0,
-            612.0,
-            967.0,
-            fill="#327421",
-            outline="")
+    def draw_image_on_canvas(self, width:float, height:int, image:PhotoImage) -> _CanvasItemId:
+        image_drawn = self.create_image(width, height, image)
         
-    def draw_piece(self, position:tuple, piece_image) -> None:
-        self.create_image(position, image = piece_image)
+        return image_drawn
 
-    def draw_button(self, position:tuple, button_image) -> None:
-        self.create_image(position, image = button_image)
+    def create_players_info(self) -> tuple[_CanvasItemId, _CanvasItemId]:
+        local_player_info = self.create_rectangle(989.0, 80.0, 1194.0, 756.0, fill="#FFFFFF", outline="")
+        remote_player_info = self.create_rectangle(1214.0, 80.0, 1419.0, 756.0, fill="#FFFFFF", outline="")
 
-    def draw_text(self, position:tuple, text:str, font:str, fill:str) -> None:
-        self.create_text(position, text = text, font = font, fill = fill)
+        return local_player_info, remote_player_info
 
-    def draw_line(self, position:tuple, position2:tuple, fill:str, width:int) -> None:
-        self.create_line(position, position2, fill = fill, width = width)
+    def write_player_texts(self) -> tuple[_CanvasItemId, _CanvasItemId]:
+        local_player_text = self.write_on_canvas(1007.0, 105.0, anchor="nw", text="You", fill="#000000", font=("Inter", 32 * -1))
+        remote_player_text = self.write_on_canvas(1238.0, 104.0, anchor="nw", text="Opponent", fill="#000000", font=("Inter", 32 * -1))
+        
+        return local_player_text, remote_player_text
+
+    def write_players_pieces_in_hand(self) -> tuple[_CanvasItemId, _CanvasItemId]:
+        local_player_pieces_in_hand = self.write_on_canvas(1007.0, 241.0, anchor="nw", text="Pieces In Hand: 12", fill="#000000", font=("Inter", 20 * -1))
+        remote_player_pieces_in_hand = self.write_on_canvas(1238.0, 241.0, anchor="nw", text="Pieces In Hand: 12", fill="#000000", font=("Inter", 20 * -1))
+
+        return local_player_pieces_in_hand, remote_player_pieces_in_hand
+    
+    def write_players_captured_pieces(self) -> tuple[_CanvasItemId, _CanvasItemId]:
+        local_player_captured_pieces = self.write_on_canvas(1007.0, 385.0, anchor="nw", text="Captured Pieces: 0", fill="#000000", font=("Inter", 20 * -1))
+        remote_player_captured_pieces = self.write_on_canvas(1238.0, 385.0, anchor="nw", text="Captured Pieces: 0", fill="#000000", font=("Inter", 20 * -1))
+
+        return local_player_captured_pieces, remote_player_captured_pieces
+
+    def write_on_canvas(self, width:float, height:int, anchor:str, text:str, fill:str, font:tuple[str, int]) -> None:
+        written_text = self.create_text(width, height, anchor = anchor, text = text, fill = fill, font = font)
+
+        return written_text
+
+    def change_text(self, text:str, written_text:_CanvasItemId) -> None:
+        self.itemconfig(written_text, text = text)
+
+    def draw_team_images(self, local_image:PhotoImage, remote_image:PhotoImage) -> _CanvasItemId:
+        self.draw_image_on_canvas(1050.0, 591.0, local_image = GameImageHandler.VASCO_piece_image) #Change later
+        self.draw_image_on_canvas(1275.0, 591.0, remote_image = GameImageHandler.AVAI_piece_image) #Change later
+
+        return
