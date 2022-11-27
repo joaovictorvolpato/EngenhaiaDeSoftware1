be577e43fa837ae6f05c41201aab039094694e5f
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-27 05:12:28 -0300

    Getters and setters plus notify, ask and update

--- TrilhaAlema/src/Interface/PlayerInterface.py
+++ TrilhaAlema/src/Interface/PlayerInterface.py
@@ -1,26 +1,48 @@
-from tkinter import  Tk, Menu, FALSE
 from Interface.InterfaceGameBoardSetter import InterfaceGameBoardSetter
 from Interface.GameImageHandler import GameImageHandler
+from Interface.InterfaceUpdater import InterfaceUpdater
+from tkinter import  Tk, Menu, FALSE
 from tkinter import messagebox
 from tkinter import simpledialog
 from Dog.dog_interface import DogPlayerInterface
 from Dog.dog_actor import DogActor
 from Game.Move import Move
 from Game.Board import Board
+from Game.Player import Player
 
 class PlayerInterface(DogPlayerInterface):
     def __init__(self) -> None:
         self.__window = self.__create_window()
         GameImageHandler.set_game_images()
-        self.__board: Board = Board() #Start program in modelling
+        self.__local_player, self.__remote_player = self.__create_players()
+        self.__interface_updater: InterfaceUpdater = InterfaceUpdater() #Implement.
+        self.__board: Board = Board(self.__local_player, self.__remote_player, self, self.__interface_updater) #Start program in modelling
         self.__interface_game_board_setter = InterfaceGameBoardSetter(self.__window, self.__board)
         self.__interface_game_board_setter.set_game_board()
         self.__window.resizable(False, False)
-        self.__menubar = self.__create_menubar()
-        self.__menu_file = self.__create_menu_file()
-        self.__add_menu_commands()
+        self.__menubar, self.__menu_file = self.__create_menu()
         self.__dog_server_interface: DogActor = self.__connect_to_dog()
         self.__window.mainloop()
+
+    @property
+    def window(self) -> Tk:
+        return self.__window
+
+    @property
+    def interface_updater(self) -> InterfaceUpdater:
+        return self.__interface_updater
+
+    @property
+    def local_player(self) -> Player:
+        return self.__local_player
+
+    @property
+    def remote_player(self) -> Player:
+        return self.__remote_player
+
+    @property
+    def dog_server_interface(self) -> DogActor:
+        return self.__dog_server_interface
 
     def __create_window(self) -> Tk:
         window = Tk()
@@ -30,6 +52,19 @@ class PlayerInterface(DogPlayerInterface):
         
         return window
 
+    def __create_players(self) -> tuple[Player, Player]:
+        local_player = Player(1, "", False, "VASCO")
+        remote_player = Player(2, "", False, "AVAI")
+
+        return local_player, remote_player
+
+    def __create_menu(self) -> tuple[Menu, Menu]:
+        menubar = self.__create_menubar()
+        menu_file = self.__create_menu_file()
+        self.__add_menu_commands(menu_file)
+
+        return menubar, menu_file
+    
     def __create_menubar(self) -> Menu:
         menubar = Menu(self.__window)
         menubar.option_add('*tearOff', FALSE)
@@ -43,12 +78,13 @@ class PlayerInterface(DogPlayerInterface):
 
         return menu_file
 
-    def __add_menu_commands(self) -> None:
-        self.__menu_file.add_command(label='Iniciar jogo', command=self.start_match)
-        self.__menu_file.add_command(label='restaurar estado inicial', command=self.start_game)
+    def __add_menu_commands(self, menu_file: Menu) -> None:
+        menu_file.add_command(label='Iniciar jogo', command=self.start_match)
+        menu_file.add_command(label='restaurar estado inicial', command=self.start_game)
 
     def __connect_to_dog(self) -> DogActor:
         player_name = simpledialog.askstring(title="Player identification", prompt="Qual o seu nome?")
+        self.__local_player.name = player_name
         dog_server_interface = DogActor()
         message = self.dog_server_interface.initialize(player_name, self)
         messagebox.showinfo(message=message)
@@ -69,6 +105,15 @@ class PlayerInterface(DogPlayerInterface):
 
     def send_move(self, move: Move) -> None:
         self.__dog_server_interface.send_move(move)
-    
+
     def receive_move(self, move: Move) -> None:
         pass
+
+    def notify_player(self, message: str) -> None:
+        messagebox.showinfo(message=message)
+
+    def update_interface_image(self) -> None:
+        self.__interface_updater.update_interface_image()
+    
+    def ask_user_accepts_draw(self) -> bool:
+        return messagebox.askyesno(message="Do you accept to end the match in a draw?")
