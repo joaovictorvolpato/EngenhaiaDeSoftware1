5b3bd66a6e0bb3ec0e8c3198408245c3eb2faf6f
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-26 22:39:51 -0300

    Cleaned main.py and defined src path

--- TrilhaAlema/src/main.py
+++ TrilhaAlema/src/main.py
@@ -1,3 +1,11 @@
+import os
 from Game.Game import Game
+from Config.Path import Path
 
-game = Game()
+def get_src_path() -> str:
+    return os.getcwd()
+
+if __name__ == "__main__":
+    src_path = get_src_path()
+    Path.set_src_path(src_path)
+    game = Game()
