b3e211dcd0601ddeb255c7ead8cf203e68afa7fe
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-26 23:10:18 -0300

    Added draw button image in code, NOT in assets

--- TrilhaAlema/src/Interface/GameImageHandler.py
+++ TrilhaAlema/src/Interface/GameImageHandler.py
@@ -9,16 +9,12 @@ class GameImageHandler:
     button_image = None
     AVAI_piece_image = None
     VASCO_piece_image = None
-    current_piece_image = None
+    DRAW_BUTTON_image = None
 
     @classmethod
     def set_game_images(cls) -> None:
         cls.canvas_image = PhotoImage(file=Path.relative_to_assets("canvas_image.png"))
         cls.button_image = PhotoImage(file = Path.relative_to_assets("button_image.png"))
         cls.AVAI_piece_image = PhotoImage(file=Path.relative_to_assets("AVAI.png"))
-        cls.VASCO_piece_image = PhotoImage(file = Path.relative_to_assets("VASCO.png"))   
-        cls.current_piece_image = cls.VASCO_piece_image
-
-    @classmethod
-    def set_current_piece_image(cls, piece_image:PhotoImage) -> None:
-        cls.current_piece_image = piece_image
+        cls.VASCO_piece_image = PhotoImage(file = Path.relative_to_assets("VASCO.png"))
+        cls.DRAW_BUTTON_image = PhotoImage(file = Path.relative_to_assets("draw_button.png"))   
