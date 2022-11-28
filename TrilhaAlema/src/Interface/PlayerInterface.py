d9eabcc0019bdddfb6ffb55d5906959fbb1ed02f
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-28 02:55:37 -0300

    Transfered some responsabilities to Game

--- TrilhaAlema/src/Interface/PlayerInterface.py
+++ TrilhaAlema/src/Interface/PlayerInterface.py
@@ -1,28 +1,28 @@
-import os, signal
-from Interface.InterfaceGameBoardSetter import InterfaceGameBoardSetter
 from Interface.GameImageHandler import GameImageHandler
 from Interface.InterfaceUpdater import InterfaceUpdater
+from Interface.InterfaceGameBoard import InterfaceGameBoard
+from Abstractions.AbstractGame import AbstractGame
 from tkinter import  Tk, Menu, FALSE
 from tkinter import messagebox
 from tkinter import simpledialog
 from Dog.dog_interface import DogPlayerInterface
 from Dog.dog_actor import DogActor
 from Game.Move import Move
-from Game.Board import Board
 from Game.Player import Player
 
 class PlayerInterface(DogPlayerInterface):
-    def __init__(self) -> None:
+    def __init__(self, game_ref) -> None:
+        self.__game: AbstractGame = game_ref
         self.__window = self.__create_window()
         GameImageHandler.set_game_images()
-        self.__local_player, self.__remote_player = self.__create_players()
-        self.__board: Board = Board(self.__local_player, self.__remote_player, self) #Start program in modelling
-        self.__interface_game_board_setter = InterfaceGameBoardSetter(self.__board)
-        self.__interface_game_board_setter.set_game_board()
+        self.__interface_game_board = InterfaceGameBoard(self.__game.board)
         self.__window.resizable(False, False)
         self.__menubar, self.__menu_file = self.__create_menu()
         self.__dog_server_interface: DogActor = self.__connect_to_dog()
-        self.__window.mainloop()
+
+    @property
+    def game(self):
+        return self.__game
 
     @property
     def window(self) -> Tk:
@@ -34,11 +34,15 @@ class PlayerInterface(DogPlayerInterface):
 
     @property
     def local_player(self) -> Player:
-        return self.__local_player
+        return self.__game.local_player
 
     @property
     def remote_player(self) -> Player:
-        return self.__remote_player
+        return self.__game.remote_player
+
+    @property
+    def interface_game_board(self) -> InterfaceGameBoard:
+        return self.__interface_game_board
 
     @property
     def dog_server_interface(self) -> DogActor:
@@ -52,19 +56,13 @@ class PlayerInterface(DogPlayerInterface):
         
         return window
 
-    def __create_players(self) -> tuple[Player, Player]:
-        local_player = Player(1, "", True, "VASCO") # CHANGE LATER
-        remote_player = Player(2, "", False, "AVAI")
-
-        return local_player, remote_player
-
     def __create_menu(self) -> tuple[Menu, Menu]:
         menubar = self.__create_menubar()
         menu_file = self.__create_menu_file(menubar)
         self.__add_menu_commands(menu_file)
 
         return menubar, menu_file
-    
+
     def __create_menubar(self) -> Menu:
         menubar = Menu(self.__window)
         menubar.option_add('*tearOff', FALSE)
@@ -83,7 +81,7 @@ class PlayerInterface(DogPlayerInterface):
 
     def __connect_to_dog(self) -> DogActor:
         player_name = simpledialog.askstring(title="Player identification", prompt="Qual o seu nome?")
-        self.__local_player.name = player_name
+        self.__game.local_player.name = player_name
         dog_server_interface = DogActor()
         message = dog_server_interface.initialize(player_name, self)
         messagebox.showinfo(message=message)
@@ -114,10 +112,16 @@ class PlayerInterface(DogPlayerInterface):
         messagebox.showinfo(message=message)
 
     def update_interface_image(self) -> None:
-        self.__interface_updater.update_interface_image()
+        InterfaceUpdater.update_interface_image(self)
     
     def ask_user_accepts_draw(self) -> bool:
         return messagebox.askyesno(message="Do you accept to end the match in a draw?")
 
-    def end_program(self) -> None:
-        os.kill(os.getpid(), signal.SIGINT)
+    def get_player_from_id(self, player_id: int) -> Player:
+        try:
+            if self.__game.local_player.id == player_id:
+                return self.__game.local_player
+            elif self.__game.remote_player.id == player_id:
+                return self.__game.remote_player
+        except ValueError:
+            print("Invalid player id")
