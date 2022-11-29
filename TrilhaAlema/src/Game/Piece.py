78f376dc4de57a5cbb1ad18fb98babf3a3dbcb54
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-11-29 17:40:07 -0300

    fix

--- TrilhaAlema/src/Game/Piece.py
+++ TrilhaAlema/src/Game/Piece.py
@@ -63,8 +63,8 @@ class Piece():
 
 		elif moinho and player != None:
 			for connection in self.__position.connections:
-				is_moinho = connection.is_moinho(player)
-				if is_moinho:
+				_is_moinho = connection.is_moinho(player)
+				if _is_moinho:
 					connection.set_positions_in_moinho(True)
 
 	@property
